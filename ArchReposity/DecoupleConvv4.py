b582595f4720a39527ac543423e07d28a4e538be
z3575522 <1337681981@qq.com>
2023-09-04 15:34:43 +0800

    Decouple Shuffle Trial3

--- ArchReposity/DecoupleConvv4.py
+++ ArchReposity/DecoupleConvv4.py
@@ -3,66 +3,6 @@ from torch import nn
 import torch.nn.functional as F
 import numpy as np
 
-class ShuffleV2Block(nn.Module):
-    def __init__(self, inp, oup, mid_channels, *, ksize, stride):
-        super(ShuffleV2Block, self).__init__()
-        self.stride = stride
-        assert stride in [1, 2]
-
-        self.mid_channels = mid_channels
-        self.ksize = ksize
-        pad = ksize // 2
-        self.pad = pad
-        self.inp = inp
-
-        outputs = oup - inp
-
-        branch_main = [
-            # pw
-            nn.Conv2d(inp, mid_channels, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            # dw
-            nn.Conv2d(mid_channels, mid_channels, ksize, stride, pad, groups=mid_channels, bias=False),
-            nn.BatchNorm2d(mid_channels),
-            # pw-linear
-            nn.Conv2d(mid_channels, outputs, 1, 1, 0, bias=False),
-            nn.BatchNorm2d(outputs),
-            nn.ReLU(inplace=True),
-        ]
-        self.branch_main = nn.Sequential(*branch_main)
-
-        if stride == 2:
-            branch_proj = [
-                # dw
-                nn.Conv2d(inp, inp, ksize, stride, pad, groups=inp, bias=False),
-                nn.BatchNorm2d(inp),
-                # pw-linear
-                nn.Conv2d(inp, inp, 1, 1, 0, bias=False),
-                nn.BatchNorm2d(inp),
-                nn.ReLU(inplace=True),
-            ]
-            self.branch_proj = nn.Sequential(*branch_proj)
-        else:
-            self.branch_proj = None
-
-    def forward(self, old_x):
-        if self.stride==1:
-            x_proj, x = self.channel_shuffle(old_x)
-            return torch.cat((x_proj, self.branch_main(x)), 1)
-        elif self.stride==2:
-            x_proj = old_x
-            x = old_x
-            return torch.cat((self.branch_proj(x_proj), self.branch_main(x)), 1)
-
-    def channel_shuffle(self, x):
-        batchsize, num_channels, height, width = x.data.size()
-        assert (num_channels % 4 == 0)
-        x = x.reshape(batchsize * num_channels // 2, 2, height * width)
-        x = x.permute(1, 0, 2)
-        x = x.reshape(2, -1, num_channels // 2, height, width)
-        return x[0], x[1]
-
 
 class ShuffleV1Block(nn.Module):
     def __init__(self, inp, oup, *, group, first_group, mid_channels, ksize, stride):
@@ -180,7 +120,7 @@ class DeCoupleConvv4(nn.Module):
 
 
 if __name__ == "__main__":
-
+    from torchsummary import summary
     def getModelSize(model):
         param_size = 0
         param_sum = 0
@@ -196,3 +136,8 @@ if __name__ == "__main__":
         print(f"Total size of the {model.__class__.__name__} :{all_size:.3f} MB")
         return (param_size, param_sum, buffer_size, buffer_sum, all_size)
 
+    dim = 48
+    model = DeCoupleConvv4(dim = 8*dim, ffn_expansion_factor=2.4)
+    model.to('cuda')
+    summary(model,(8*dim,128,128),batch_size=1)
+    getModelSize(model)
