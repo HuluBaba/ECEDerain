a8aed9001d9f593b1ee67251f078226c5e9b3ecd
z3575522 <1337681981@qq.com>
2023-11-15 23:57:58 +0800

    NEW: nlq to 1gt

--- basicsr/data/data_util.py
+++ basicsr/data/data_util.py
@@ -250,6 +250,52 @@ def paired_paths_from_folder(folders, keys, filename_tmpl):
                   (f'{gt_key}_path', gt_path)]))
     return paths
 
+def nlq_rain1200_from_folder(folders, keys, filename_tmpl):
+    """Generate paired paths from folders.
+
+    Args:
+        folders (list[str]): A list of folder path. The order of list should
+            be [input_folder, gt_folder].
+        keys (list[str]): A list of keys identifying folders. The order should
+            be in consistent with folders, e.g., ['lq', 'gt'].
+        filename_tmpl (str): Template for each filename. Note that the
+            template excludes the file extension. Usually the filename_tmpl is
+            for files in the input folder. USELESS HERE
+
+    Returns:
+        list[str]: Returned path list.
+    """
+    assert len(folders) == 2, (
+        'The len of folders should be 2 with [input_folder, gt_folder]. '
+        f'But got {len(folders)}')
+    assert len(keys) == 2, (
+        'The len of keys should be 2 with [input_key, gt_key]. '
+        f'But got {len(keys)}')
+    input_folder, gt_folder = folders
+    input_key, gt_key = keys
+
+    input_paths = list(scandir(input_folder))
+    gt_paths = list(scandir(gt_folder))
+    # assert len(input_paths) == len(gt_paths), (
+    #     f'{input_key} and {gt_key} datasets have different number of images: '
+    #     f'{len(input_paths)}, {len(gt_paths)}.')
+    paths = []
+    for idx in range(len(input_paths)):
+        input_path = input_paths[idx]
+        basename, ext = osp.splitext(osp.basename(input_path))
+        # gt_path = gt_paths[idx]
+        # basename_gt, ext_gt = osp.splitext(osp.basename(gt_path))
+        gt_name = f'{basename.split("_")[0]}{ext}'
+        gt_path = osp.join(gt_folder, gt_name)
+        assert gt_name in gt_paths, (f'{gt_name} is not in '
+                                           f'{gt_key}_paths.')
+        input_path = osp.join(input_folder, input_path)
+        paths.append(
+            dict([(f'{input_key}_path', input_path),
+                  (f'{gt_key}_path', gt_path)]))
+    return paths
+
+
 def paired_DP_paths_from_folder(folders, keys, filename_tmpl):
     """Generate paired paths from folders.
 
--- basicsr/data/paired_image_dataset.py
+++ basicsr/data/paired_image_dataset.py
@@ -4,7 +4,8 @@ from torchvision.transforms.functional import normalize
 from basicsr.data.data_util import (paired_paths_from_folder,
                                     paired_DP_paths_from_folder,
                                     paired_paths_from_lmdb,
-                                    paired_paths_from_meta_info_file)
+                                    paired_paths_from_meta_info_file,
+                                    nlq_rain1200_from_folder)
 from basicsr.data.transforms import augment, paired_random_crop, paired_random_crop_DP, random_augmentation
 from basicsr.utils import FileClient, imfrombytes, img2tensor, padding, padding_DP, imfrombytesDP
 
@@ -67,6 +68,14 @@ class Dataset_PairedImage(data.Dataset):
             self.paths = paired_paths_from_meta_info_file(
                 [self.lq_folder, self.gt_folder], ['lq', 'gt'],
                 self.opt['meta_info_file'], self.filename_tmpl)
+        elif 'sp_path_func' in self.opt:
+            if self.opt['sp_path_func'] == 'nlq_rain1200_from_folder':
+                self.paths = nlq_rain1200_from_folder(
+                    [self.lq_folder, self.gt_folder], ['lq', 'gt'],
+                    self.filename_tmpl)
+            else:
+                raise Exception("no sp_path_func named {}".format(self.opt['sp_path_func']))
+
         else:
             self.paths = paired_paths_from_folder(
                 [self.lq_folder, self.gt_folder], ['lq', 'gt'],
